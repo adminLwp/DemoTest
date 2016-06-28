e6b3222148430d53793fba476d43ff1d9ab07369
adminLwp <137259856@qq.com>
2016-06-28 09:09:24 +0800

    ee

--- src/sfsfsfssfas/asdasd.java
+++ src/sfsfsfssfas/asdasd.java
@@ -4,7 +4,7 @@ public class asdasd {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
- int i =4; String q,zz,zzz,zzzzz,zzzzzz; int s,d,c,z; 
+ int i,q,w,e,r,a,d,f,x,z=0; 
 	}
 
 }
